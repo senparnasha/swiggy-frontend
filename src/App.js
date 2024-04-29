b666b59ba9a4d116723431e9e770faa84d085b1e
senparnasha <senparnasha@gmail.com>
2024-04-29 22:01:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 
 import './App.css';
 
-const App = async ()=> {
+// const App = async ()=> {
+const App = ()=> {
   
-const data=  await fetch('http://localhost:3001/view/resturents')
+// const data=  await fetch('http://localhost:3001/view/resturents')
 
-const res= await data.json()
-console.log(res)
+// const res= await data.json()
+// console.log(res)
 
 
   return (
     <div className="App">
-      Hello World
-    
+      <h1>Hello World</h1>
     </div>
   );
 }
