253a083b80673ceab0714c766af8113a0854dcf9
senparnasha <senparnasha@gmail.com>
2024-05-11 15:44:30 +0530

    get all resturents

--- src/components/ViewResturents.jsx
+++ src/components/ViewResturents.jsx
@@ -1,56 +1,127 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { useEffect, useState } from "react";
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import axios from "axios";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from "@mui/material";
+import DeleteModal from "./modal/DeleteModal";
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70, align: 'center', headerAlign: 'center' }, 
-  { field: 'firstName', headerName: 'First name', width: 130,align: 'center', headerAlign: 'center'  },
-  { field: 'lastName', headerName: 'Last name', width: 130, align: 'center', headerAlign: 'center' },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-    align: 'center',
-    headerAlign: 'center' 
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    align: 'center', 
-    headerAlign: 'center',
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
-];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
 
 export default function ViewResturents() {
+  const [rows, setRows] = useState([]);
+  const [open,setOpen]= useState(false)
+
+
+  const columns = [
+    {
+      field: "sl_no",
+      headerName: "Sl. No",
+      width: 130,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      width: 130,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      width: 90,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "phn_no",
+      headerName: "Phone Number",
+      width: 160,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "costing",
+      headerName: "Costing",
+      width: 160,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 150,
+      align: "center",
+      headerAlign: "center",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <div>
+            {/* Edit icon */}
+            <IconButton onClick={() => handleEdit(params.row.id)}>
+              <EditIcon />
+            </IconButton>
+            {/* Delete icon */}
+            <IconButton onClick={() => handleDelete(params.row.id)}>
+              <DeleteIcon />
+            </IconButton>
+          </div>
+        );
+      },
+    },
+  ];
+  
+  const handleEdit=()=>{
+  
+  }
+  
+  const handleDelete=()=>{
+    console.log("modal")
+    setOpen(true)
+  }
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3001/resturent/view/all"
+        );
+        console.log("data", response.data.rows);
+
+        let rowData = response.data.rows;
+
+        let filteredData = rowData.map((row, index) => {
+          return { ...row, sl_no: index + 1 };
+        });
+
+        setRows(filteredData)
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{  width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 100 },
           },
         }}
         pageSizeOptions={[5, 10]}
-        
       />
+      <DeleteModal open={open} handleClose={()=>setOpen(false)}/>
+    
     </div>
   );
 }
---
+++ src/components/modal/DeleteModal.jsx
@@ -0,0 +1,54 @@
+
+import * as React from 'react';
+import {Box, Typography} from '@mui/material';
+import Button from '@mui/material/Button';
+
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const DeleteModal = ({open, handleClose}) => {
+
+  const handleCancel=()=>{
+    handleClose()
+  }
+  return (
+    
+         <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h5" component="h2" >
+           Are you sure you want to Delete?
+          </Typography>
+          
+          <Box display='flex' justifyContent='right' alignItems='center' gap={2} mt={2}>
+          <Button variant='contained' mt={1} onClick={handleCancel}>Cancel</Button>
+          <Button variant='contained' mt={1}>Delete</Button>
+          </Box>
+        
+
+          </Box>
+          </div>
+       
+      
+      </Modal>
+   
+  )
+}
+
+export default DeleteModal
